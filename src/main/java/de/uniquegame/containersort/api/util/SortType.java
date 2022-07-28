c4f9f4a9c98df2a2df394f749a05aa89ecfcd215
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-07-29 00:01:26 +0200

    Removed placeholder enum constant

--- src/main/java/de/uniquegame/containersort/api/util/SortType.java
+++ src/main/java/de/uniquegame/containersort/api/util/SortType.java
@@ -1,4 +1,4 @@
-package de.delta203.chestsort.api.util;
+package de.uniquegame.containersort.api.util;
 
 import com.google.common.collect.Maps;
 import org.jetbrains.annotations.NotNull;
@@ -8,16 +8,26 @@ import java.util.Map;
 
 public enum SortType {
 
-    PLACEHOLDER,
-    ALPHABETICALLY,
-    HIGHEST_ITEM_AMOUNT,
-    LOWEST_ITEM_AMOUNT;
+    ALPHABETICALLY("Alphabetically"),
+    HIGHEST_ITEM_AMOUNT("Highest Amount"),
+    LOWEST_ITEM_AMOUNT("Lowest Amount");
 
     public static final Map<String, SortType> BY_NAME = Maps.newHashMap();
+
+    private final String displayName;
+
+    SortType(@NotNull String displayName) {
+        this.displayName = displayName;
+    }
 
     @Nullable
     public static SortType getSortType(@NotNull String name) {
         return BY_NAME.get(name);
+    }
+
+    @NotNull
+    public String getDisplayName() {
+        return this.displayName;
     }
 
     static {
