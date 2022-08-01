4f49c02730daaa3700aaa6e4ff973a403c12b5fe
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-08-01 23:23:44 +0200

    Added UUIDTagType

---
+++ src/main/java/de/uniquegame/containersort/api/UUIDTagType.java
@@ -0,0 +1,37 @@
+package de.uniquegame.containersort.api;
+
+import org.bukkit.persistence.PersistentDataAdapterContext;
+import org.bukkit.persistence.PersistentDataType;
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.LongBuffer;
+import java.util.UUID;
+
+public class UUIDTagType implements PersistentDataType<long[], UUID> {
+
+    @Override
+    public @NotNull Class<long[]> getPrimitiveType() {
+        return long[].class;
+    }
+
+    @Override
+    public @NotNull Class<UUID> getComplexType() {
+        return UUID.class;
+    }
+
+    @Override
+    public long @NotNull [] toPrimitive(@NotNull UUID complex, @NotNull PersistentDataAdapterContext context) {
+        LongBuffer bb = LongBuffer.wrap(new long[2]);
+        bb.put(complex.getMostSignificantBits());
+        bb.put(complex.getLeastSignificantBits());
+        return bb.array();
+    }
+
+    @Override
+    public @NotNull UUID fromPrimitive(long @NotNull [] primitive, @NotNull PersistentDataAdapterContext context) {
+        LongBuffer byteBuffer = LongBuffer.wrap(primitive);
+        long firstLong = byteBuffer.get();
+        long secondLong = byteBuffer.get();
+        return new UUID(firstLong, secondLong);
+    }
+}
