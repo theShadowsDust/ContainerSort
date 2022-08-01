ef8619b97c4eebbaf59723d8a1c8efe2971b1402
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-08-01 23:21:17 +0200

    Cleaning up code but not finished yet

--- src/main/java/de/uniquegame/containersort/listener/PlayerInteractListener.java
+++ src/main/java/de/uniquegame/containersort/listener/PlayerInteractListener.java
@@ -3,8 +3,11 @@ package de.uniquegame.containersort.listener;
 import de.uniquegame.containersort.api.ContainerSortApi;
 import de.uniquegame.containersort.api.util.InventoryUtil;
 import de.uniquegame.containersort.api.util.SortType;
-import org.bukkit.block.*;
-import org.bukkit.block.data.Rotatable;
+import org.bukkit.block.Block;
+import org.bukkit.block.Container;
+import org.bukkit.block.Sign;
+import org.bukkit.block.data.BlockData;
+import org.bukkit.block.data.Directional;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
@@ -31,29 +34,33 @@ public final class PlayerInteractListener implements Listener {
 
         if (clickedBlock.getState() instanceof Sign sign) {
 
-            if(!this.containerSortApi.isSortSign(sign)) return;
-            if(!this.containerSortApi.isSignOwner(player, sign)) {
-                player.sendMessage(this.containerSortApi.getLanguageService().getMessage("invalid-sign-owner", player));
-                return;
-            }
+            if (!this.containerSortApi.isSortSign(sign)) return;
+            BlockData blockData = sign.getBlockData();
 
-            if (sign.getBlockData() instanceof Rotatable rotatable) {
-                Block attachedBlock = clickedBlock.getRelative(rotatable.getRotation());
+            if (blockData instanceof Directional directional) {
+
+                Block attachedBlock = sign.getBlock().getRelative(directional.getFacing().getOppositeFace());
                 if (attachedBlock.getState() instanceof Container container) {
 
-                    if (!isValidContainer(container)) return;
+                    if (!this.containerSortApi.isValidContainer(container)) return;
+
                     if (!player.hasPermission("container.sort.allow")) return;
                     if (this.containerSortApi.getSettings().isWorldDisabled(clickedBlock.getWorld())) return;
 
+                    if (!this.containerSortApi.isSignOwner(player.getUniqueId(), sign) &&
+                            !player.hasPermission("containersort.sort.admin")) {
 
-                    String sortTypeName = this.containerSortApi.getLanguageService().stripColors(sign.line(1));
-                    SortType sortType = SortType.getSortType(sortTypeName);
+                        player.sendMessage(this.containerSortApi.getLanguageService().getMessage(
+                                "invalid-sign-owner", player,
+                                this.containerSortApi.getLanguageService().prefix()));
+                        return;
+                    }
 
+                    SortType sortType = SortType.findSortType(sign.lines());
                     if (sortType == null) {
                         player.sendMessage(this.containerSortApi.getLanguageService().getMessage(
                                 "cannot-find-sort-type",
-                                player,
-                                sortTypeName));
+                                player, this.containerSortApi.getLanguageService().prefix()));
                         return;
                     }
 
@@ -63,9 +70,5 @@ public final class PlayerInteractListener implements Listener {
                 }
             }
         }
-    }
-
-    private boolean isValidContainer(@NotNull Container container) {
-        return container instanceof Chest || container instanceof Barrel || container instanceof DoubleChest;
     }
 }
