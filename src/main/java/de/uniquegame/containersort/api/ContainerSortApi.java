7f1f98d54417d8d8875755729060c0740057a386
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-08-01 23:16:22 +0200

    Added method isValidContainer

--- src/main/java/de/uniquegame/containersort/api/ContainerSortApi.java
+++ src/main/java/de/uniquegame/containersort/api/ContainerSortApi.java
@@ -1,24 +1,26 @@
 package de.uniquegame.containersort.api;
 
 import de.uniquegame.containersort.ContainerSortPlugin;
-import de.uniquegame.containersort.api.util.SortType;
 import de.uniquegame.containersort.configuration.ContainerSortPluginConfiguration;
 import de.uniquegame.containersort.configuration.ContainerSortSettings;
 import de.uniquegame.containersort.service.LanguageService;
 import de.uniquegame.containersort.service.PersistentDataStoreService;
+import org.bukkit.block.BlockState;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
 
 public interface ContainerSortApi {
 
     boolean isSortSign(Sign sign);
 
     /**
-     * @param player the player who interacts with the attached {@link org.bukkit.block.Sign}
+     * @param playerId the player who interacts with the attached {@link org.bukkit.block.Sign}
      * @return true if the {@link Player} can sort the {@link org.bukkit.block.Container}
      */
-    boolean isSignOwner(@NotNull Player player, @NotNull Sign sign);
+    boolean isSignOwner(@NotNull UUID playerId, @NotNull Sign sign);
 
     /**
      * @return the {@link ContainerSortPlugin}
@@ -33,5 +35,7 @@ public interface ContainerSortApi {
 
     ContainerSortSettings getSettings();
 
-    void saveSignData(@NotNull Player player, @NotNull Sign sign, @NotNull SortType sortType);
+    void saveSignData(@NotNull Player player, @NotNull Sign sign);
+
+    boolean isValidContainer(@NotNull BlockState state);
 }
