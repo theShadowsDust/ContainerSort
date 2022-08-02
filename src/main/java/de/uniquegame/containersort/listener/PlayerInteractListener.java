c2dbdd1e4b30fb56b47382219922cacf0a1b7b53
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-08-02 22:19:53 +0200

    Cleaning up code

--- src/main/java/de/uniquegame/containersort/listener/PlayerInteractListener.java
+++ src/main/java/de/uniquegame/containersort/listener/PlayerInteractListener.java
@@ -2,13 +2,10 @@ package de.uniquegame.containersort.listener;
 
 import de.uniquegame.containersort.api.ContainerSortApi;
 import de.uniquegame.containersort.api.util.InventoryUtil;
+import de.uniquegame.containersort.api.util.SignUtil;
 import de.uniquegame.containersort.api.util.SortType;
-import org.bukkit.block.Block;
-import org.bukkit.block.Container;
+import de.uniquegame.containersort.service.LanguageService;
 import org.bukkit.block.Sign;
-import org.bukkit.block.data.BlockData;
-import org.bukkit.block.data.Directional;
-import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,57 +15,64 @@ import org.jetbrains.annotations.NotNull;
 public final class PlayerInteractListener implements Listener {
 
     private final ContainerSortApi containerSortApi;
+    private final LanguageService languageService;
 
     public PlayerInteractListener(@NotNull ContainerSortApi containerSortApi) {
         this.containerSortApi = containerSortApi;
+        this.languageService = containerSortApi.getLanguageService();
     }
 
     @EventHandler
     public void handlePlayerInteract(PlayerInteractEvent event) {
 
+        if (!event.getAction().isRightClick()) return;
         if (event.useInteractedBlock() == Event.Result.DENY) return;
-        Block clickedBlock = event.getClickedBlock();
+        var clickedBlock = event.getClickedBlock();
 
         if (clickedBlock == null) return;
-        Player player = event.getPlayer();
+        var player = event.getPlayer();
 
         if (clickedBlock.getState() instanceof Sign sign) {
 
             if (!this.containerSortApi.isSortSign(sign)) return;
-            BlockData blockData = sign.getBlockData();
+            var prefix = this.containerSortApi.getLanguageService().prefix();
 
-            if (blockData instanceof Directional directional) {
+            var container = SignUtil.findConnectedContainer(sign);
+            if (container == null) {
 
-                Block attachedBlock = sign.getBlock().getRelative(directional.getFacing().getOppositeFace());
-                if (attachedBlock.getState() instanceof Container container) {
+                SignUtil.breakSign(player, this.languageService.getMessage(
+                        "no-connected-container-found",
+                        player, prefix),
+                        sign);
 
-                    if (!this.containerSortApi.isValidContainer(container)) return;
-
-                    if (!player.hasPermission("container.sort.allow")) return;
-                    if (this.containerSortApi.getSettings().isWorldDisabled(clickedBlock.getWorld())) return;
-
-                    if (!this.containerSortApi.isSignOwner(player.getUniqueId(), sign) &&
-                            !player.hasPermission("containersort.sort.admin")) {
-
-                        player.sendMessage(this.containerSortApi.getLanguageService().getMessage(
-                                "invalid-sign-owner", player,
-                                this.containerSortApi.getLanguageService().prefix()));
-                        return;
-                    }
-
-                    SortType sortType = SortType.findSortType(sign.lines());
-                    if (sortType == null) {
-                        player.sendMessage(this.containerSortApi.getLanguageService().getMessage(
-                                "cannot-find-sort-type",
-                                player, this.containerSortApi.getLanguageService().prefix()));
-                        return;
-                    }
-
-                    InventoryUtil.sortContainer(sortType, container.getInventory());
-                    player.playSound(player.getLocation(),
-                            this.containerSortApi.getSettings().getSortSuccessSound(), 1F, 1F);
-                }
+                return;
             }
+
+            if (!this.containerSortApi.isValidContainer(container)) return;
+
+            if (!player.hasPermission("container.sort.allow")) return;
+            if (this.containerSortApi.getSettings().isWorldDisabled(clickedBlock.getWorld())) return;
+
+            if (!this.containerSortApi.isSignOwner(player.getUniqueId(), sign) &&
+                    !player.hasPermission("containersort.sort.admin")) {
+
+                player.sendMessage(this.containerSortApi.getLanguageService().getMessage(
+                        "invalid-sign-owner", player,
+                        this.containerSortApi.getLanguageService().prefix()));
+                return;
+            }
+
+            var sortType = SortType.findSortType(sign.lines());
+            if (sortType == null) {
+                player.sendMessage(this.containerSortApi.getLanguageService().getMessage(
+                        "cannot-find-sort-type",
+                        player, this.containerSortApi.getLanguageService().prefix()));
+                return;
+            }
+
+            InventoryUtil.sortContainer(sortType, container.getInventory());
+            player.playSound(player.getLocation(),
+                    this.containerSortApi.getSettings().getSortSuccessSound(), 1F, 1F);
         }
     }
 }
