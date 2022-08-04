e684f6da9b230efe302e9677bc69c14761832b9e
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-08-04 21:29:39 +0200

    Added new command and fixed edit sign command

--- src/main/java/de/uniquegame/containersort/command/ContainerSortCommand.java
+++ src/main/java/de/uniquegame/containersort/command/ContainerSortCommand.java
@@ -4,7 +4,10 @@ import cloud.commandframework.annotations.CommandDescription;
 import cloud.commandframework.annotations.CommandMethod;
 import cloud.commandframework.annotations.CommandPermission;
 import de.uniquegame.containersort.api.ContainerSortApi;
+import de.uniquegame.containersort.util.Permissions;
+import de.uniquegame.containersort.util.SignUtil;
 import org.bukkit.block.Block;
+import org.bukkit.block.Container;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
@@ -17,12 +20,16 @@ public class ContainerSortCommand {
         this.containerSortApi = containerSortApi;
     }
 
-    @CommandMethod("containersort language")
-    @CommandPermission("containersort.command.languagecode")
-    @CommandDescription("Get your client language code")
-    public void executeLanguageCodeCommand(Player player) {
-        player.sendMessage(this.containerSortApi.getLanguageService().getMessage("client-language-code", player,
-                this.containerSortApi.getLanguageService().prefix(), player.locale()));
+    @CommandMethod("containersort add language")
+    @CommandPermission("containersort.command.addlanguage")
+    @CommandDescription("Create a new language")
+    public void addLanguageCommand(Player player) {
+        if (this.containerSortApi.getLanguageService().createLanguage(player.locale())) {
+            player.sendMessage(this.containerSortApi.getLanguageService().getMessage(
+                    "language-successfully-created",
+                    player,
+                    this.containerSortApi.getLanguageService().prefix(), player.locale()));
+        }
     }
 
     @CommandMethod("containersort editsign")
@@ -36,6 +43,14 @@ public class ContainerSortCommand {
             return;
         }
 
+        Container container = SignUtil.findConnectedContainer(sign);
+        if(container == null || !this.containerSortApi.isValidContainer(container.getBlock().getState())) {
+            SignUtil.breakSign(player, this.containerSortApi.getLanguageService().getMessage(
+                    "no-connected-container-found", player,
+                    this.containerSortApi.getLanguageService().prefix()), sign);
+            return;
+        }
+
         if (!this.containerSortApi.isSortSign(sign)) {
             player.sendMessage(this.containerSortApi.getLanguageService().getMessage("invalid-sign", player,
                     this.containerSortApi.getLanguageService().prefix()));
@@ -43,9 +58,11 @@ public class ContainerSortCommand {
         }
 
         if (!this.containerSortApi.isSignOwner(player.getUniqueId(), sign)) {
-            player.sendMessage(this.containerSortApi.getLanguageService().getMessage("invalid-sign-owner", player,
-                    this.containerSortApi.getLanguageService().prefix()));
-            return;
+            if (!player.hasPermission(Permissions.PERMISSION_SORT_CREATE_OTHERS)) {
+                player.sendMessage(this.containerSortApi.getLanguageService().getMessage("invalid-sign-owner", player,
+                        this.containerSortApi.getLanguageService().prefix()));
+                return;
+            }
         }
 
         player.openSign(sign);
