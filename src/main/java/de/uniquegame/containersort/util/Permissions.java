1251a76cc1e4722062c3e6e42be15ad6121e5a6c
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-08-04 21:30:16 +0200

    Added Permissions class

---
+++ src/main/java/de/uniquegame/containersort/util/Permissions.java
@@ -0,0 +1,12 @@
+package de.uniquegame.containersort.util;
+
+public final class Permissions {
+
+    public static final String PERMISSION_SORT_ALLOW = "container.sort.allow";
+    public static final String PERMISSION_SORT_ALLOW_OTHERS = "container.sort.allow.others";
+
+    public static final String PERMISSION_SORT_CREATE = "container.sort.create";
+    public static final String PERMISSION_SORT_CREATE_OTHERS = "container.sort.create.others";
+
+    public static final String PERMISSION_SORT_BREAK_OTHERS = "container.sort.break.others";
+}
