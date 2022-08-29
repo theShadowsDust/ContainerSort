e9e3ad4b2136406c95ea5aff6d3560dfcce4682e
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-08-29 17:25:41 +0200

    Fix code smells

--- src/main/java/de/uniquegame/containersort/util/Permissions.java
+++ src/main/java/de/uniquegame/containersort/util/Permissions.java
@@ -2,11 +2,15 @@ package de.uniquegame.containersort.util;
 
 public final class Permissions {
 
-    public static final String PERMISSION_SORT_ALLOW = "container.sort.allow";
-    public static final String PERMISSION_SORT_ALLOW_OTHERS = "container.sort.allow.others";
+    public static final String PERMISSION_SORT_ALLOW = "containersort.allow";
+    public static final String PERMISSION_SORT_ALLOW_OTHERS = "containersort.allow.others";
 
-    public static final String PERMISSION_SORT_CREATE = "container.sort.create";
-    public static final String PERMISSION_SORT_CREATE_OTHERS = "container.sort.create.others";
+    public static final String PERMISSION_SORT_CREATE = "containersort.create";
+    public static final String PERMISSION_SORT_CREATE_OTHERS = "containersort.create.others";
 
-    public static final String PERMISSION_SORT_BREAK_OTHERS = "container.sort.break.others";
+    public static final String PERMISSION_SORT_BREAK_OTHERS = "containersort.break.others";
+
+    private Permissions() {
+        throw new IllegalStateException("Utility class");
+    }
 }
