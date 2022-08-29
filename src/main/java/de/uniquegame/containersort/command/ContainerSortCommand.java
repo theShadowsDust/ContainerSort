b016f41115c21686c0159bd93cbf9b566a03c87d
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-08-29 17:22:20 +0200

    Improve base checks

--- src/main/java/de/uniquegame/containersort/command/ContainerSortCommand.java
+++ src/main/java/de/uniquegame/containersort/command/ContainerSortCommand.java
@@ -4,6 +4,7 @@ import cloud.commandframework.annotations.CommandDescription;
 import cloud.commandframework.annotations.CommandMethod;
 import cloud.commandframework.annotations.CommandPermission;
 import de.uniquegame.containersort.api.ContainerSortApi;
+import de.uniquegame.containersort.service.LanguageService;
 import de.uniquegame.containersort.util.Permissions;
 import de.uniquegame.containersort.util.SignUtil;
 import org.bukkit.block.Block;
@@ -15,20 +16,22 @@ import org.jetbrains.annotations.NotNull;
 public class ContainerSortCommand {
 
     private final ContainerSortApi containerSortApi;
+    private final LanguageService languageService;
+    private final String pluginPrefix;
 
     public ContainerSortCommand(@NotNull ContainerSortApi containerSortApi) {
         this.containerSortApi = containerSortApi;
+        this.languageService = containerSortApi.getLanguageService();
+        this.pluginPrefix = languageService.pluginPrefix();
     }
 
     @CommandMethod("containersort add language")
     @CommandPermission("containersort.command.addlanguage")
     @CommandDescription("Create a new language")
     public void addLanguageCommand(Player player) {
-        if (this.containerSortApi.getLanguageService().createLanguage(player.locale())) {
-            player.sendMessage(this.containerSortApi.getLanguageService().getMessage(
-                    "language-successfully-created",
-                    player,
-                    this.containerSortApi.getLanguageService().prefix(), player.locale()));
+        if (this.languageService.createLanguage(player.locale())) {
+            player.sendMessage(this.languageService.getMessage(
+                    "language-successfully-created", player, this.pluginPrefix, player.locale()));
         }
     }
 
@@ -38,31 +41,25 @@ public class ContainerSortCommand {
 
         Block targetBlock = player.getTargetBlock(this.containerSortApi.getSettings().getMaxSignDistance());
         if (targetBlock == null || !(targetBlock.getState() instanceof Sign sign)) {
-            player.sendMessage(this.containerSortApi.getLanguageService().getMessage("no-sign-in-line-of-sight",
-                    player, this.containerSortApi.getLanguageService().prefix()));
+            player.sendMessage(this.languageService.getMessage("no-sign-in-line-of-sight", player, this.pluginPrefix));
             return;
         }
 
         Container container = SignUtil.findConnectedContainer(sign);
         if(container == null || !this.containerSortApi.isValidContainer(container.getBlock().getState())) {
-            SignUtil.breakSign(player, this.containerSortApi.getLanguageService().getMessage(
-                    "no-connected-container-found", player,
-                    this.containerSortApi.getLanguageService().prefix()), sign);
+            player.sendMessage(this.languageService.getMessage("no-connected-container-found", player, this.pluginPrefix));
             return;
         }
 
         if (!this.containerSortApi.isSortSign(sign)) {
-            player.sendMessage(this.containerSortApi.getLanguageService().getMessage("invalid-sign", player,
-                    this.containerSortApi.getLanguageService().prefix()));
+            player.sendMessage(this.languageService.getMessage("invalid-sign", player, this.pluginPrefix));
             return;
         }
 
-        if (!this.containerSortApi.isSignOwner(player.getUniqueId(), sign)) {
-            if (!player.hasPermission(Permissions.PERMISSION_SORT_CREATE_OTHERS)) {
-                player.sendMessage(this.containerSortApi.getLanguageService().getMessage("invalid-sign-owner", player,
-                        this.containerSortApi.getLanguageService().prefix()));
-                return;
-            }
+        if (!this.containerSortApi.isSignOwner(player.getUniqueId(), sign) &&
+                !player.hasPermission(Permissions.PERMISSION_SORT_CREATE_OTHERS)) {
+            player.sendMessage(this.languageService.getMessage("invalid-sign-owner", player, this.pluginPrefix));
+            return;
         }
 
         player.openSign(sign);
