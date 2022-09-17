607f22e24653dbcd409e17fe3ed9a3a888f60e2c
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-09-17 18:27:47 +0200

    Remove method findPlayerId and fix code smells

--- src/main/java/de/uniquegame/containersort/util/SignUtil.java
+++ src/main/java/de/uniquegame/containersort/util/SignUtil.java
@@ -9,7 +9,6 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.logging.Level;
@@ -28,29 +27,6 @@ public final class SignUtil {
 
     private SignUtil() {
         throw new IllegalStateException("Utility class");
-    }
-
-    @Nullable
-    public static UUID findPlayerId(@NotNull Sign sign) {
-
-        CompletableFuture<UUID> future = CompletableFuture.supplyAsync(() -> {
-            UUID playerId = null;
-            for (int i = 0; i < sign.lines().size() && playerId == null; i++) {
-                var uuid = Bukkit.getPlayerUniqueId(MessageUtil.stripColors(sign.line(i)));
-                if (uuid == null) continue;
-                playerId = uuid;
-            }
-
-            return playerId;
-        }, ContainerSortApiImpl.EXECUTOR_SERVICE);
-
-        try {
-            return future.get();
-        } catch (InterruptedException | ExecutionException e) {
-            Bukkit.getLogger().log(Level.SEVERE, "[ContainerSort] Cannot find player id", e);
-            Thread.currentThread().interrupt();
-            return null;
-        }
     }
 
     @Nullable
@@ -89,10 +65,12 @@ public final class SignUtil {
             String playerName = null;
             for (int i = 0; i < lines.size() && playerName == null; i++) {
                 var line = MessageUtil.stripColors(lines.get(i));
-                var offlinePlayer = Bukkit.getOfflinePlayerIfCached(line);
-                if (offlinePlayer == null || offlinePlayer.getName() != null && offlinePlayer.getName().equalsIgnoreCase(line))
-                    continue;
-                playerName = offlinePlayer.getName();
+                if (!line.isEmpty()) {
+                    var offlinePlayer = Bukkit.getOfflinePlayer(line);
+                    if (offlinePlayer.hasPlayedBefore() || offlinePlayer.getName() != null && offlinePlayer.getName().equalsIgnoreCase(line))
+                        continue;
+                    playerName = offlinePlayer.getName();
+                }
             }
 
             return playerName;
