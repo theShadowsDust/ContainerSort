fc230bff1b83ef9fc768f3ab520b9f21434b84a3
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-10-04 19:12:30 +0200

    Add debug messages and null check

--- src/main/java/de/uniquegame/containersort/ContainerSortPlugin.java
+++ src/main/java/de/uniquegame/containersort/ContainerSortPlugin.java
@@ -10,21 +10,20 @@ import cloud.commandframework.paper.PaperCommandManager;
 import de.uniquegame.containersort.api.ContainerSortApi;
 import de.uniquegame.containersort.api.ContainerSortApiImpl;
 import de.uniquegame.containersort.command.ContainerSortCommand;
-import de.uniquegame.containersort.configuration.ContainerSortSettings;
 import de.uniquegame.containersort.listener.ContainerProtectionListener;
 import de.uniquegame.containersort.listener.PlayerInteractListener;
 import de.uniquegame.containersort.listener.SignListener;
 import org.bukkit.command.CommandSender;
-import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.IOException;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.logging.Level;
 
-public class ContainerSortPlugin extends JavaPlugin {
+public final class ContainerSortPlugin extends JavaPlugin {
 
     private ContainerSortApiImpl containerSortApi;
 
@@ -32,6 +31,7 @@ public class ContainerSortPlugin extends JavaPlugin {
     public void onLoad() {
         Optional<Integer> javaVersionOptional = Runtime.version().build();
         if (javaVersionOptional.orElse(8) < 17) {
+            getLogger().log(Level.WARNING, "Your Java Version is outdated! Please use 17 or newer");
             this.getServer().getPluginManager().disablePlugin(this);
         }
     }
@@ -39,28 +39,32 @@ public class ContainerSortPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
 
-        saveDefaultConfig();
-        ConfigurationSerialization.registerClass(ContainerSortSettings.class);
         PluginManager pluginManager = getServer().getPluginManager();
+        try {
 
-        this.containerSortApi = new ContainerSortApiImpl(this);
-        this.getServer().getServicesManager().register(
-                ContainerSortApi.class,
-                this.containerSortApi,
-                this,
-                ServicePriority.Highest);
+            this.containerSortApi = new ContainerSortApiImpl(this);
+            this.getServer().getServicesManager().register(
+                    ContainerSortApi.class,
+                    this.containerSortApi,
+                    this,
+                    ServicePriority.Highest);
 
+            pluginManager.registerEvents(new PlayerInteractListener(this.containerSortApi), this);
+            pluginManager.registerEvents(new ContainerProtectionListener(this.containerSortApi), this);
+            pluginManager.registerEvents(new SignListener(this.containerSortApi), this);
+            buildCommandSystem();
 
-        pluginManager.registerEvents(new PlayerInteractListener(this.containerSortApi), this);
-        pluginManager.registerEvents(new ContainerProtectionListener(this.containerSortApi), this);
-        pluginManager.registerEvents(new SignListener(this.containerSortApi), this);
-        buildCommandSystem();
+        } catch (IOException e) {
+            pluginManager.disablePlugin(this);
+            getLogger().log(Level.SEVERE, "Something went wrong on load configuartion", e);
+        }
     }
 
     @Override
     public void onDisable() {
-        this.getServer().getServicesManager().unregisterAll(this);
-        ConfigurationSerialization.unregisterClass(ContainerSortSettings.class);
+        if (this.containerSortApi != null) {
+            this.getServer().getServicesManager().unregister(this.containerSortApi);
+        }
     }
 
     private void buildCommandSystem() {
