59fe62ce8270522af378bdaa7296e60bab067a7b
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-10-04 19:09:37 +0200

    Add reload config and feedback for the language command

--- src/main/java/de/uniquegame/containersort/command/ContainerSortCommand.java
+++ src/main/java/de/uniquegame/containersort/command/ContainerSortCommand.java
@@ -4,16 +4,22 @@ import cloud.commandframework.annotations.CommandDescription;
 import cloud.commandframework.annotations.CommandMethod;
 import cloud.commandframework.annotations.CommandPermission;
 import de.uniquegame.containersort.api.ContainerSortApi;
+import de.uniquegame.containersort.configuration.ContainerSortProperty;
 import de.uniquegame.containersort.service.LanguageService;
 import de.uniquegame.containersort.util.Permissions;
 import de.uniquegame.containersort.util.SignUtil;
 import org.bukkit.block.Block;
 import org.bukkit.block.Container;
 import org.bukkit.block.Sign;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-public class ContainerSortCommand {
+import java.io.IOException;
+import java.util.logging.Level;
+
+@SuppressWarnings("unused")
+public final class ContainerSortCommand {
 
     private final ContainerSortApi containerSortApi;
     private final LanguageService languageService;
@@ -25,21 +31,45 @@ public class ContainerSortCommand {
         this.pluginPrefix = languageService.pluginPrefix();
     }
 
-    @CommandMethod("containersort add language")
-    @CommandPermission("containersort.command.addlanguage")
-    @CommandDescription("Create a new language")
-    public void addLanguageCommand(Player player) {
-        if (this.languageService.createLanguage(player.locale())) {
-            player.sendMessage(this.languageService.getMessage(
-                    "language-successfully-created", player, this.pluginPrefix, player.locale()));
+
+    @CommandMethod("containersort|cs reload config")
+    @CommandPermission("containersort.command.reload.config")
+    @CommandDescription("Reload the configuration file")
+    public void reloadConfigCommand(CommandSender commandSender) {
+        try {
+            this.containerSortApi.reloadConfig();
+            this.containerSortApi.getLanguageService().load();
+            commandSender.sendMessage(this.languageService.getMessage("config-reload-successfully", commandSender));
+        } catch (IOException e) {
+            commandSender.sendMessage(this.languageService.getMessage("cannot-reload-config", commandSender));
+            this.containerSortApi.getPlugin().getLogger().log(Level.SEVERE, e.getMessage(), e);
         }
     }
 
-    @CommandMethod("containersort editsign")
+    @CommandMethod("containersort|cs add language")
+    @CommandPermission("containersort.command.addlanguage")
+    @CommandDescription("Create a new language")
+    public void addLanguageCommand(Player player) {
+        var locale = player.locale();
+        this.languageService.updateLocale(locale, false).thenAcceptAsync(localeUpdateResult -> {
+            switch (localeUpdateResult) {
+                case CANNOT_SAVE -> player.sendMessage(this.languageService.getMessage(
+                        "language-cannot-save", player, this.pluginPrefix, locale));
+                case ALREADY_EXISTS -> player.sendMessage(this.languageService.getMessage(
+                        "language-already-exists", player, this.pluginPrefix, locale));
+                default -> player.sendMessage(this.languageService.getMessage(
+                        "language-successfully-created", player, this.pluginPrefix, locale));
+            }
+        });
+    }
+
+    @SuppressWarnings("java:S1874")
+    @CommandMethod("containersort|cs editsign")
     @CommandPermission("containersort.command.editsign")
     public void executeEditSignCommand(@NotNull Player player) {
 
-        Block targetBlock = player.getTargetBlock(this.containerSortApi.getSettings().getMaxSignDistance());
+        Block targetBlock = player.getTargetBlock(this.containerSortApi.
+                getPropertyValue(ContainerSortProperty.MAX_SIGN_DISTANCE, Double.class).intValue());
         if (targetBlock == null || !(targetBlock.getState() instanceof Sign sign)) {
             player.sendMessage(this.languageService.getMessage("no-sign-in-line-of-sight", player, this.pluginPrefix));
             return;
