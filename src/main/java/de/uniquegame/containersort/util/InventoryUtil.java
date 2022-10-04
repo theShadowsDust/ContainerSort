23e65f427359b973ca6edd5d8af2fcceaf01e944
Dustin Weckenbrock <dustin.weckenbrock@gmx.de>
2022-10-04 19:04:39 +0200

    Move method sortContainer to other class

--- src/main/java/de/uniquegame/containersort/util/InventoryUtil.java
+++ src/main/java/de/uniquegame/containersort/util/InventoryUtil.java
@@ -1,51 +1,23 @@
 package de.uniquegame.containersort.util;
 
-import de.uniquegame.containersort.api.SortType;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 
 public final class InventoryUtil {
 
-    private static final Comparator<ItemStack> SORT_ALPHABETICALLY = Comparator.comparing(itemStack -> itemStack.getType().name());
-    private static final Comparator<ItemStack> SORT_BY_LOWEST_AMOUNT = Comparator.comparing(ItemStack::getAmount);
-    private static final Comparator<ItemStack> SORT_BY_HIGHEST_AMOUNT = SORT_BY_LOWEST_AMOUNT.reversed();
+    public static final Comparator<ItemStack> SORT_ALPHABETICALLY = Comparator.comparing(itemStack -> itemStack.getType().name());
+    public static final Comparator<ItemStack> SORT_BY_LOWEST_AMOUNT = Comparator.comparing(ItemStack::getAmount);
+    public static final Comparator<ItemStack> SORT_BY_HIGHEST_AMOUNT = SORT_BY_LOWEST_AMOUNT.reversed();
 
     private InventoryUtil() {
         throw new IllegalStateException("Utility class");
     }
 
-    public static void sortContainer(@NotNull SortType sortType, @NotNull Inventory inventory) {
-
-        List<ItemStack> list = new ArrayList<>();
-        for (ItemStack content : inventory.getContents()) {
-            if (content != null) {
-                list.add(content);
-            }
-        }
-
-        switch (sortType) {
-            case ALPHABETICALLY -> list.sort(SORT_ALPHABETICALLY);
-            case HIGHEST_ITEM_AMOUNT -> list.sort(SORT_BY_HIGHEST_AMOUNT);
-            case LOWEST_ITEM_AMOUNT -> list.sort(SORT_BY_LOWEST_AMOUNT);
-            default -> throw new IllegalStateException("Unexpected value: " + sortType);
-        }
-
-        // Erstelle ein ItemStack Array mit der Länge von der Liste siehe oben.
-        ItemStack[] itemStacks = new ItemStack[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            itemStacks[i] = list.get(i);
-        }
-
-        // Setze den content von dem Inventar (container) dann braucht man das Inventar nicht leeren.
-        inventory.setContents(InventoryUtil.mergeSimilarStacks(itemStacks));
-    }
-
+    @SuppressWarnings({"java:S1119", "java:S135"})
     @NotNull
     public static ItemStack[] mergeSimilarStacks(ItemStack... items) {
         if (items.length <= 1) {
@@ -54,10 +26,13 @@ public final class InventoryUtil {
 
         List<ItemStack> itemList = new LinkedList<>();
 
+        Iterating:
         for (ItemStack item : items) {
             for (ItemStack iStack : itemList) {
-                if (iStack.getAmount() != iStack.getMaxStackSize() && item.isSimilar(iStack)) {
+                if (iStack.getAmount() == iStack.getMaxStackSize()) continue;
+                if (item.isSimilar(iStack)) {
                     iStack.setAmount(iStack.getAmount() + item.getAmount());
+                    continue Iterating;
                 }
             }
 
